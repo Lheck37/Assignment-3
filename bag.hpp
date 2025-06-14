236859decd581395751350eb6673ac61fd5be5aa
Lheck37 <lheck@my.athens.edu>
2025-06-14 15:29:00 -0500

    Create bag.hpp

---
+++ bag.hpp
@@ -0,0 +1,21 @@
+#ifndef BAG_HPP
+#define BAG_HPP
+
+#include "list.hpp"
+
+template <typename T>
+class bag {
+private:
+    list<T> items;
+
+public:
+    void add(const T& item) {
+        items.push_back(item);
+    }
+
+    void show() const {
+        items.print();
+    }
+};
+
+#endif // BAG_HPP
